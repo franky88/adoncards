9ced0faab507c47c6c777b158714196d82bade10
Frank Ramos <ramosfp99@gmail.com>
2021-10-25 13:54:55 +0800

    updates

--- cards/urls.py
+++ cards/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cards/', include('cardadds.urls'))
+    path('/', include('cardadds.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
